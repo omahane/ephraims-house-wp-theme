2987078fb9ff6b3ccca4a4200447d38928b62173
Christian Burk <christian@creighton.edu>
2018-04-14 07:13:54 -0600

    Working on header

---
+++ mission-style.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Template Name: Mission Style
+**/
+get_header();
+?>
+    <div class="container">
+        <div class="row">         
+            <div class="col-md-12 col-sm-12 col-xs-12 blog-article">
+                <?php while ( have_posts() ) : the_post(); ?>
+                        <?php the_content();
+                        wp_link_pages( array(
+                               'before' => '<div class="page-links">' . esc_html__( 'Page:', 'college-education' ),
+                               'after'  => '</div>',
+                            ) );
+                        ?>
+                <?php endwhile;  ?> 
+            </div>            
+        </div>
+    </div>
+<?php get_footer(); ?>
