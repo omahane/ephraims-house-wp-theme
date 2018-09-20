e95c145596d22e931602894e75d2e657706b8300
Christian Burk <cmb@christianburk.com>
2018-09-19 19:35:42 -0500

    Form style changes

---
+++ private-art-session-form-js.js
@@ -0,0 +1,11 @@
+<script>
+
+jQuery(document).ready(function() {
+	console.log("wrkng?");
+	var mybutton = document.querySelector("button#submit");
+	mybutton.classList.add("btn-primary");
+	mybutton.innerHTML="Sign Up";
+});
+
+	</script>
+	
